1df3bb929088dc8988161c741c99dc34a28a55e2
Tyler Hancock <darklime@live.ca>
2018-07-12 12:16:26 -0600

    Added the mod and shows an example of how to use the logger.

---
+++ Bookcase/BookcaseMod.cs
@@ -0,0 +1,20 @@
+﻿using StardewModdingAPI;
+
+namespace Bookcase {
+
+    public class BookcaseMod : Mod {
+
+        internal static IModHelper modHelper;
+        internal static IReflectionHelper reflection;
+        internal static Log logger;
+
+        public override void Entry(IModHelper helper) {
+
+            modHelper = helper;
+            reflection = helper.Reflection;
+            logger = new Log(this);
+
+            logger.Info("Succesffuly loaded!");
+        }
+    }
+}

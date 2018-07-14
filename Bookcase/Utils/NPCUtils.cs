0cf039f17d6dc8a414586186540991d221a362ef
Mira <mattfdavies12@gmail.com>
2018-07-14 03:42:41 +0200

    Bug fixes
    I made stupid mistakes with some of my position checks, they're fixed now. Confirmed working with RelationshipTooltips using the methods.

--- Bookcase/Utils/NPCUtils.cs
+++ Bookcase/Utils/NPCUtils.cs
@@ -10,7 +10,9 @@ namespace Bookcase.Utils
 {
     public class NPCUtils
     {
-        #region GetNPCUnderCursor
+        /// <summary>
+        /// Cache for NPC tileLocation - reduces double call of NPC location calculation to a single call.
+        /// </summary>
         private static Vector2 npcTileLoc;
         /// <summary>
         /// Gets all NPC Characters in the player's current location
@@ -19,33 +21,37 @@ namespace Bookcase.Utils
         {
             get
             {
-                return Game1.player.currentLocation.getCharacters();
+                if (Game1.gameMode == Game1.playingGameMode)
+                    return Game1.player.currentLocation.getCharacters();
+                return null;
             }
         }
         /// <summary>
-        /// Checks if an NPC is on the same tile as the mouse, with the option of checking the tile above aswell. (Adults span 2 tiles)
+        /// Checks if an NPC is on the same tile as the mouse, with the option of checking the tile above aswell. (Most NPCs fill 2 tiles, visually)
         /// </summary>
         /// <param name="checkOneTileAbove"></param>
         /// <returns></returns>
         public static bool IsNPCOnMouseTile(bool checkOneTileAbove = false)
         {
-            return (npcTileLoc == Game1.currentCursorTile || checkOneTileAbove && npcTileLoc == Game1.currentCursorTile - Vector2.UnitY);
+            return Game1.currentCursorTile == npcTileLoc || (checkOneTileAbove && Game1.currentCursorTile == (npcTileLoc - Vector2.UnitY));
         }
         /// <summary>
         /// Tries to get an NPC which is on the tile under the mouse cursor.
-        /// Depending on the NPC's Age, the method will also look at one tile above the NPC's tile.
         /// </summary>
         /// <param name="npc">The NPC under the cursor, or null if none is found.</param>
         /// <returns>True if there is an NPC present, False if there is not.</returns>
         public static bool TryGetNPCUnderCursor(out NPC npc)
         {
-            foreach(NPC n in LocationCharacters)
+            if (Game1.gameMode == Game1.playingGameMode)
             {
-                npcTileLoc = n.getTileLocation();
-                if (IsNPCOnMouseTile(n.Age != NPC.child))
+                foreach (NPC n in LocationCharacters)
                 {
-                    npc = n;
-                    return true;
+                    npcTileLoc = n.getTileLocation();
+                    if (IsNPCOnMouseTile(true))
+                    {
+                        npc = n;
+                        return true;
+                    }
                 }
             }
             npc = null;
@@ -53,44 +59,44 @@ namespace Bookcase.Utils
         }
         /// <summary>
         /// Tries to get an NPC (filtered by some specified filter) on the tile under the mouse cursor.
-        /// Depending on the NPC's Age, the method will also look at one tile above the NPC's tile.
         /// </summary>
         /// <param name="npc">The NPC present under the mouse cursor or null</param>
         /// <param name="filter">The filter to be applied when searching for a </param>
-        /// <returns>True if there is a filtered NPC present, False if there is not, or the filter removes the NPC present</returns>
+        /// <returns>True if there is a filtered NPC present, False if there is not, or the filter removes the NPC present.</returns>
         public static bool TryGetNPCUnderCursor(out NPC npc, Func<NPC, bool> filter)
         {
             bool b = TryGetNPCsUnderCursor(out List<NPC> toFilter, filter);
             if (b)
-                npc = toFilter.First();
+                npc = toFilter.First();//First element of the List
             else
                 npc = null;
             return b;
         }
         /// <summary>
         /// Tries to get a list of NPCs on the tile under the mouse cursor.
-        /// Depending on the NPC's Age, the method will also look at one tile above the NPC's tile.
         /// </summary>
-        /// <param name="npcs"></param>
-        /// <returns></returns>
+        /// <param name="npcs">A List of the NPCs present under the mouse.</param>
+        /// <returns>True if there is an NPC present, False if there is not.</returns>
         public static bool TryGetNPCsUnderCursor(out List<NPC> npcs)
         {
             npcs = new List<NPC>();
-            foreach (NPC n in LocationCharacters)
+            if (Game1.gameMode == Game1.playingGameMode)
             {
-                npcTileLoc = n.getTileLocation();
-                if (IsNPCOnMouseTile(n.Age != NPC.child))
+                foreach (NPC n in LocationCharacters)
                 {
-                    npcs.Add(n);
+                    npcTileLoc = n.getTileLocation();
+                    if (IsNPCOnMouseTile(true))
+                    {
+                        npcs.Add(n);
+                    }
                 }
             }
             return npcs.Count > 0;
         }
         /// <summary>
         /// Tries to get a List of NPCs (filtered by some specified filter) on the tile under the mouse cursor.
-        /// Depending on the NPC's Age, the method will also look at one tile above the NPC's tile.
         /// </summary>
-        /// <param name="npc">The NPCs present under the mouse cursor or null</param>
+        /// <param name="npc">A List of the NPCs present under the mouse, filtered by the given filter.</param>
         /// <param name="filter">The filter to be applied when searching for a </param>
         /// <returns>True if there is a filtered NPC present, False if there is not, or the filter removes the NPC present</returns>
         public static bool TryGetNPCsUnderCursor(out List<NPC> npc, Func<NPC, bool> filter)
@@ -102,6 +108,5 @@ namespace Bookcase.Utils
             npc = toFilter.Where(filter).ToList();
             return npc.Count > 0;
         }
-        #endregion
     }
 }
